d873773d26ede3289df0224fe238f5166c83eb20
Benjamin Smith <bsmith@cabbage.org.uk>
2022-10-25 22:46:26 +0100

    Abstract out a 'passenger_queue' as both Bus and BusStop need one.

--- src/bus.py
+++ src/bus.py
@@ -1,27 +1,29 @@
+from .passenger_set import PassengerSet
+
 class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
-        self.passengers = []
+        self.passengers = PassengerSet()
 
     def drive(self):
         return "Brum brum"
 
     def passenger_count(self):
-        return len(self.passengers)
+        return self.passengers.count_passengers()
 
     def pick_up(self, person):
-        self.passengers.append(person)
+        self.passengers.add_to_set(person)
 
     def drop_off(self, person):
-        self.passengers.remove(person)
+        self.passengers.remove_from_set(person)
 
     def empty(self):
-        for person in self.passengers:
+        for person in self.passengers.get_passenger_list():
             self.drop_off(person)
 
     def pick_up_from_stop(self, bus_stop):
-        new_passengers = bus_stop.queue
+        new_passengers = bus_stop.queue.get_passenger_list()
         bus_stop.clear()
         for person in new_passengers:
             self.pick_up(person)
--- src/bus_stop.py
+++ src/bus_stop.py
@@ -1,13 +1,15 @@
+from .passenger_set import PassengerSet
+
 class BusStop:
     def __init__(self, name):
         self.name = name
-        self.queue = []
+        self.queue = PassengerSet()
 
     def queue_length(self):
-        return len(self.queue)
+        return self.queue.count_passengers()
 
     def add_to_queue(self, person):
-        self.queue.append(person)
+        self.queue.add_to_set(person)
 
     def clear(self):
-        self.queue = []
+        self.queue.clear()
