9a3cd5e2f1b93c139b648f3e4ea83d87fcb4307a
Benjamin Smith <bsmith@cabbage.org.uk>
2022-10-26 09:05:30 +0100

    Rewrite using delegation.

--- src/bus.py
+++ src/bus.py
@@ -1,32 +1,36 @@
-from .passenger_set import PassengerSet
 from .delegatetools import delegate
 
 class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
-        self.passengers = PassengerSet()
+        self.passengers = set()
 
     def drive(self):
         return "Brum brum"
 
+    # def passenger_count(self):
+    #     return self.passengers.count_passengers()
+    # passenger_count = delegate("passengers", "length")
     def passenger_count(self):
-        return self.passengers.count_passengers()
+        return len(self.passengers)
 
     # def pick_up(self, person):
     #     self.passengers.add_to_set(person)
-    pick_up = delegate("passengers", "add_to_set")
+    pick_up = delegate("passengers", "add")
 
-    def drop_off(self, person):
-        self.passengers.remove_from_set(person)
+    # def drop_off(self, person):
+    #     self.passengers.remove_from_set(person)
+    drop_off = delegate("passengers", "remove")
 
-    def empty(self):
-        for person in self.passengers.get_passenger_list():
-            self.drop_off(person)
+    # def empty(self):
+    #     for person in self.passengers.get_passenger_list():
+    #         self.drop_off(person)
+    empty = delegate("passengers", "clear")
 
     def pick_up_from_stop(self, bus_stop):
-        new_passengers = bus_stop.queue.get_passenger_list()
-        bus_stop.clear()
+        new_passengers = bus_stop.queue
         for person in new_passengers:
             self.pick_up(person)
+        bus_stop.clear()
         
--- src/bus_stop.py
+++ src/bus_stop.py
@@ -1,15 +1,17 @@
-from .passenger_set import PassengerSet
+from .delegatetools import delegate
 
 class BusStop:
     def __init__(self, name):
         self.name = name
-        self.queue = PassengerSet()
+        self.queue = list()
 
     def queue_length(self):
-        return self.queue.count_passengers()
+        return len(self.queue)
 
-    def add_to_queue(self, person):
-        self.queue.add_to_set(person)
+    # def add_to_queue(self, person):
+    #     self.queue.add_to_set(person)
+    add_to_queue = delegate("queue", "append")
 
-    def clear(self):
-        self.queue.clear()
+    # def clear(self):
+    #     self.queue.clear()
+    clear = delegate("queue", "clear")
